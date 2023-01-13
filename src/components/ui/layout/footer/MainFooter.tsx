8d939abe01af9914c74df377b6e4622d08bcbc5e
Abiodun Jayb <ajiboyeabiodun001@gmail.com>
2023-01-13 10:57:26 +0100

    new commit

--- src/components/ui/layout/footer/MainFooter.tsx
+++ src/components/ui/layout/footer/MainFooter.tsx
@@ -1,70 +1,64 @@
+import { footerContents } from "../../../../data";
+import { Card, LinkText, TitleText, white } from "../../styles";
 
 const MainFooter = () => {
   return (
-    <div>MainFooter</div>
-  )
-}
+    <>
+      <Card flex gap="50px">
+        {footerContents.map((item) => (
+          <Card
+            key={item.id}
+            width="250px"
+            flex
+            flexdirection="column"
+            gap="18px"
+          >
+            <TitleText
+              color="#b5b5b5"
+              weight="500"
+              size="16px"
+              lineheight="19px"
+            >
+              {item.heading}
+            </TitleText>
+            <Card flex flexdirection="column" gap="18px">
+              {item.details.map((subitem) =>
+                subitem.link === true ? (
+                  <a
+                    href={subitem.path}
+                    target="_blank"
+                    key={subitem.id}
+                    style={{
+                      width: "fit-content",
+                      color: `${white}`,
+                      fontWeight: "400",
+                      fontSize: "16px",
+                      textDecoration: "none",
+                      lineHeight: "19px",
+                    }}
+                  >
+                    {subitem.handle}
+                  </a>
+                ) : (
+                  <LinkText
+                    width="fit-content"
+                    to={subitem.path}
+                    key={subitem.id}
+                    color={white}
+                    weight="400"
+                    size="16px"
+                    lineheight="19px"
+                  >
+                    {subitem.handle}
+                  </LinkText>
+                )
+              )}
+            </Card>
+          </Card>
+        ))}
+      </Card>
+    </>
+  );
+};
 
-export default MainFooter
-// import { footerContents } from "../../../../data/Data";
-// import { Card, LinkText, TitleText, white } from "../../styles";
-
-// const MainFooter = () => {
-//   return (
-//     <>
-//       <Card flex justifycontent="space-between">
-//         {footerContents.map((item) => (
-//           <Card
-//             key={item.id}
-//             width="160px"
-//             flex
-//             flexdirection="column"
-//             gap="24px"
-//           >
-//             <TitleText
-//               color="rgba(255, 255, 255, 0.4)"
-//               weight="500"
-//               size="16px"
-//               lineheight="19px"
-//             >
-//               {item.heading}
-//             </TitleText>
-//             <Card flex flexdirection="column" gap="16px">
-//               {item.details.map((subitem) =>
-//                 subitem.link === true ? (
-//                   <a
-//                     href={subitem.path}
-//                     target="_blank"
-//                     key={subitem.id}
-//                     style={{
-//                       color: `${white}`,
-//                       fontWeight: "400",
-//                       fontSize: "16px",
-//                       textDecoration: "none",
-//                       lineHeight: "19px",
-//                     }}
-//                   >
-//                     {subitem.handle}
-//                   </a>
-//                 ) : (
-//                   <LinkText
-//                     to={subitem.path}
-//                     key={subitem.id}
-//                     color={white}
-//                     weight="400"
-//                     size="16px"
-//                     lineheight="19px"
-//                   >
-//                     {subitem.handle}
-//                   </LinkText>
-//                 )
-//               )}
-//             </Card>
-//           </Card>
-//         ))}
-//       </Card>
-//     </>
-//   );
-// };
-
-// export default MainFooter;
+export default MainFooter;
