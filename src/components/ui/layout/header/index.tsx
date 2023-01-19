99bfb8539fca76402bc71d54e7d8c4edd775b8e4
Abiodun Jayb <ajiboyeabiodun001@gmail.com>
2023-01-20 00:21:36 +0100

    mobile responsiveness implementation

--- src/components/ui/layout/header/index.tsx
+++ src/components/ui/layout/header/index.tsx
@@ -8,36 +8,41 @@ import {
   primaryBlue,
   white,
 } from "../../styles";
+import MobileHeader from "./mobileHeader/MobileHeader";
 import NavigationSection from "./navigationSection/NavigationSection";
 
 const Header = () => {
   return (
-    <MainSection width="100%">
-      <InnerSection>
-        <Card
-          flex
-          justifycontent="space-between"
-          alignitems="center"
-          padding="24px 0"
-        >
-          <LinkText to="/">
-            <Image
-              src="https://res.cloudinary.com/dsx94d7pa/image/upload/v1671781649/Mono-clone/logo_y19idq.svg"
-              alt="mono-logo"
-            />
-          </LinkText>
-          <NavigationSection />
-          <Button
-            padding="13px 20px"
-            color={white}
-            bg={primaryBlue}
-            radius="10px"
+    <>
+      <MainSection width="100%">
+        <InnerSection>
+          <Card
+            flex
+            justifycontent="space-between"
+            alignitems="center"
+            padding="24px 0"
+            mddisplay="none"
           >
-            Sign in
-          </Button>
-        </Card>
-      </InnerSection>
-    </MainSection>
+            <LinkText to="/">
+              <Image
+                src="https://res.cloudinary.com/dsx94d7pa/image/upload/v1671781649/Mono-clone/logo_y19idq.svg"
+                alt="mono-logo"
+              />
+            </LinkText>
+            <NavigationSection />
+            <Button
+              padding="13px 20px"
+              color={white}
+              bg={primaryBlue}
+              radius="10px"
+            >
+              Sign in
+            </Button>
+          </Card>
+        </InnerSection>
+      </MainSection>
+      <MobileHeader />
+    </>
   );
 };
 
