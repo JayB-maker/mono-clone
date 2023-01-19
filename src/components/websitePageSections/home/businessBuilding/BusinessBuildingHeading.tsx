2f1a55f5d5c726d4a3b452e6c05b9bbd040a859d
Abiodun Jayb <ajiboyeabiodun001@gmail.com>
2023-01-20 00:22:00 +0100

    mobile responsiveness implementation

--- src/components/websitePageSections/home/businessBuilding/BusinessBuildingHeading.tsx
+++ src/components/websitePageSections/home/businessBuilding/BusinessBuildingHeading.tsx
@@ -3,16 +3,37 @@ import { Card, Text, TitleText } from "../../../ui/styles";
 const BusinessBuildingHeading = () => {
   return (
     <Card flex>
-      <Card flex flexdirection="column" gap="30px" flexratio="1">
-        <TitleText size="64px" lineheight="73.6px">
+      <Card
+        flex
+        flexdirection="column"
+        gap="30px"
+        flexratio="1"
+        mdwidth="576px"
+        smwidth="100%"
+      >
+        <TitleText
+          size="64px"
+          lineheight="73.6px"
+          mdsize="48px"
+          smsize="30px"
+          mdlineheight="56px"
+          smlineheight="35px"
+        >
           Business building with Mono
         </TitleText>
-        <Text size="26px" lineheight="35.75px">
+        <Text
+          size="26px"
+          lineheight="35.75px"
+          mdsize="20px"
+          smsize="16px"
+          mdlineheight="28px"
+          smlineheight="22px"
+        >
           Here are some of the interesting features and experiences powered by
           Mono.
         </Text>
       </Card>
-      <Card flexratio="1" />
+      <Card flexratio="1" mddisplay="none" />
     </Card>
   );
 };
