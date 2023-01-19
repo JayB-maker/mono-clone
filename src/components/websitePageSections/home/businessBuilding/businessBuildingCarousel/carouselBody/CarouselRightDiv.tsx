545dd442ed19cb287ac420c26f38991109f967b8
Abiodun Jayb <ajiboyeabiodun001@gmail.com>
2023-01-20 00:22:21 +0100

    mobile responsiveness implementation

--- src/components/websitePageSections/home/businessBuilding/businessBuildingCarousel/carouselBody/CarouselRightDiv.tsx
+++ src/components/websitePageSections/home/businessBuilding/businessBuildingCarousel/carouselBody/CarouselRightDiv.tsx
@@ -11,16 +11,18 @@ const CarouselRightDiv = () => {
         (detail, index) =>
           carouselState === index && (
             <>
-              <Image src={detail.image} />
+              <Image src={detail.image} smwidth="100%" />
               <Card
                 position="absolute"
-                padding="35px"
+                padding="35px" smpadding="20px"
                 radius="50%"
                 bg={white}
                 right="20px"
                 top="-50px"
+                smtop="-30px"
+                smright="-20px"
               >
-                <Image src={detail.icon} width="50px" />
+                <Image src={detail.icon} width="50px" smwidth="40px" />
               </Card>
             </>
           )
